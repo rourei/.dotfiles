74e77cbe66150b25d5a13a47445de9a0e2bcdbab
Rouven Reichert <rourei@aol.com>
2025-10-12 21:26:53 +0200

    [Neovim] Configure tab/indentation behavior and hard wrap of lines

--- config/nvim/lua/rourei/init.lua
+++ config/nvim/lua/rourei/init.lua
@@ -1,3 +1,8 @@
 require("rourei.remap")
 print("Hello from the rourei directory!")
-
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.textwidth = 120
+vim.opt.wrap = true
