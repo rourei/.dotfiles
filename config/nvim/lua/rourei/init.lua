e3609e50a0ea6320210f3710bf190d46c5248859
Rouven Reichert <rourei@aol.com>
2025-10-13 08:53:51 +0200

    [Neovim] Various smaller config tweaks

--- config/nvim/lua/rourei/init.lua
+++ config/nvim/lua/rourei/init.lua
@@ -1,8 +1,18 @@
 require("rourei.remap")
 print("Hello from the rourei directory!")
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-vim.opt.textwidth = 120
-vim.opt.wrap = true
+
+local opt = vim.opt
+
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2 -- Number of spaces inserted when indenting
+opt.expandtab = true -- Indent using spaces instead of tab
+opt.smartindent = true
+opt.autoindent = true
+
+opt.textwidth = 120
+opt.wrap = true
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
