8fd6dcb88775ec3af0dbcb7f50d260224eb8de58
Rouven Reichert <rourei@aol.com>
2025-10-13 23:27:11 +0200

    [Neovim] Key bindings for moving current line

--- config/nvim/lua/rourei/remap.lua
+++ config/nvim/lua/rourei/remap.lua
@@ -2,4 +2,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>ft", vim.cmd.Ex)
-vim.keymap.set("i", "jk", "<Esc>", {})
+vim.keymap.set("i", "jk", "<Esc>")
+-- Moving lines, see https://vim.fandom.com/wiki/Moving_lines_up_or_down#Mappings_to_move_lines
+vim.keymap.set("n", "<C-j>", ":m+<CR>==")
+vim.keymap.set("n", "<C-k>", ":m-2<CR>==")
