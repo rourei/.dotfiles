6e19a960f842773b3d05df9b2a3d8d142629f87a
Rouven Reichert <rourei@aol.com>
2025-10-16 10:28:32 +0200

    [Neovim][Terminator] Try slightly transparent background

--- config/nvim/lua/rourei/colors.lua
+++ config/nvim/lua/rourei/colors.lua
@@ -1,6 +1,6 @@
 vim.cmd[[colorscheme habamax]]
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 function GimmeColor(color)
 	color = color or "habamax"
