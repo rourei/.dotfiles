f639a54b2b32df576ed43aea4f0714de35fda3c0
Rouven Reichert <rourei@aol.com>
2025-11-26 11:23:45 +0100

    [Bash] Add util commands for Bash script creation

--- config/nvim/lua/rourei/commands.lua
+++ config/nvim/lua/rourei/commands.lua
@@ -1,2 +1,4 @@
 -- User commands
 vim.api.nvim_create_user_command( 'Jsonf', '%!jq .', {} )
+vim.api.nvim_create_user_command( 'Shebang', ':normal! 0i#!/usr/bin/env bash<ESC>', {} )
+vim.api.nvim_create_user_command( 'ScriptDir', ':normal! 0iSCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"<ESC>', {} )
