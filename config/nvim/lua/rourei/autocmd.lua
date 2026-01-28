394655b38bfa2c7b8af8fd86fafd048d7a8b2e68
Rouven Reichert <rourei@aol.com>
2026-01-28 09:26:16 +0100

    [Neovim] Plugin updates, disable folding autocmd, fix treesitter config

--- config/nvim/lua/rourei/autocmd.lua
+++ config/nvim/lua/rourei/autocmd.lua
@@ -1,16 +1,16 @@
 local api = vim.api
 
 -- Setting the fold method based on Treesitter parser existence, see https://stackoverflow.com/a/79405264
-api.nvim_create_autocmd({ "FileType" }, {
-  callback = function()
-            if require("nvim-treesitter.parsers").has_parser() then
-                vim.opt.foldmethod = "expr"
-                vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-            else
-                vim.opt.foldmethod = "syntax"
-            end
-        end,
-    })
+-- api.nvim_create_autocmd({ "FileType" }, {
+--   callback = function()
+--             if require("nvim-treesitter.parsers").has_parser() then
+--                 vim.opt.foldmethod = "expr"
+--                 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+--             else
+--                 vim.opt.foldmethod = "syntax"
+--             end
+--         end,
+--     })
 
 api.nvim_create_autocmd("FileType", {
   pattern = { "vim" },
